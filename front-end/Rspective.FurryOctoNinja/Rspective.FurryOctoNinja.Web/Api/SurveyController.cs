a9f0c958d70205821d81051ef11dea5795527c3f
Bandro <bandraszyk@gmail.com>
2015-05-13 13:08:37 +0100

    Project basic structure

---
+++ front-end/Rspective.FurryOctoNinja/Rspective.FurryOctoNinja.Web/Api/SurveyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+
+namespace Rspective.FurryOctoNinja.Web.Api
+{
+    public class SurveyController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
