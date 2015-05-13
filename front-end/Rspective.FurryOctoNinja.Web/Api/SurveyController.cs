b748d52322db2f4dd89cf25a9ead8bff238cae53
bandro <bandraszyk@gmail.com>
2015-05-13 19:34:04 +0100

    First version of survey api

--- front-end/Rspective.FurryOctoNinja.Web/Api/SurveyController.cs
+++ front-end/Rspective.FurryOctoNinja.Web/Api/SurveyController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Http;
 using System.Net;
 using System.Net.Http;
 
 namespace Rspective.FurryOctoNinja.Web.Api
 {
-    public class SurveyController : ApiController
+    public class SurveyController : System.Web.Http.ApiController
     {
+        [HttpGet]
         public HttpResponseMessage Get()
         {
-            return Request.CreateResponse(HttpStatusCode.OK);
+            return Request.CreateResponse(HttpStatusCode.OK, "Get()");
+        }
+
+        [HttpGet]
+        [ActionName("users")]
+        public HttpResponseMessage Users()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Users()");
+        }
+
+        [HttpGet]
+        [ActionName("results")]
+        public HttpResponseMessage Results()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Results()");
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Post()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Save()");
         }
     }
 }
