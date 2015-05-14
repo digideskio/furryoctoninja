c3f4f16ce1b33fe8a10f91df06f0e526f8b03814
marcin.polak <mpolak87@gmail.com>
2015-05-14 16:59:34 +0200

    Define basic survey API

---
+++ android/data/src/main/java/pl/rspective/data/repository/SurveyRepository.java
@@ -0,0 +1,4 @@
+package pl.rspective.data.repository;
+
+public interface SurveyRepository {
+}
--- android/data/src/main/java/pl/rspective/data/rest/McKinseyGeneralApi.java
+++ android/data/src/main/java/pl/rspective/data/rest/McKinseyGeneralApi.java
@@ -1,5 +1,21 @@
 package pl.rspective.data.rest;
 
+import retrofit.http.GET;
+import retrofit.http.POST;
+import rx.Observable;
+
 public interface McKinseyGeneralApi {
+
+    @GET("/api/survey")
+    Observable<String> fetchSurvey();
+
+    @POST("/api/survey ")
+    Observable<String> submitSurvey();
+
+    @GET("/api/survey/results")
+    Observable<String> fetchSurveyResults();
+
+    @GET("/api/survey/users")
+    Observable<String> fetchSurveyUsers();
 
 }
