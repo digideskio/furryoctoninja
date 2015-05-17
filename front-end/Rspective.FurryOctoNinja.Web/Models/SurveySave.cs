79c0de74bfb91277a93860dc3581e8f22f9b177c
Bandro <bandraszyk@gmail.com>
2015-05-17 09:45:15 +0200

    Adde model for save operation

---
+++ front-end/Rspective.FurryOctoNinja.Web/Models/SurveySave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rspective.FurryOctoNinja.Web.Models
+{
+    public class SurveySave
+    {
+        public int Id { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public ICollection<SurveySaveAnswer> Answers { get; set; }
+    }
+}
---
+++ front-end/Rspective.FurryOctoNinja.Web/Models/SurveySaveAnswer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rspective.FurryOctoNinja.Web.Models
+{
+    public class SurveySaveAnswer
+    {
+        public int QuestionId { get; set; }
+
+        public int AnwserId { get; set; }
+    }
+}
