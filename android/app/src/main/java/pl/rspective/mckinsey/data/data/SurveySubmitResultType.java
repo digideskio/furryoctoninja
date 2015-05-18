25e4a21bc448a5c4fc17ae91f9936f1820d15181
marcin.polak <mpolak87@gmail.com>
2015-05-18 21:00:26 +0200

    Improve dialogs when submitting the survey - confirm, ok and error

---
+++ android/app/src/main/java/pl/rspective/mckinsey/data/data/SurveySubmitResultType.java
@@ -0,0 +1,29 @@
+package pl.rspective.mckinsey.data.data;
+
+import android.support.annotation.StringRes;
+
+import pl.rspective.mckinsey.R;
+
+public enum SurveySubmitResultType {
+
+    SURVEY_OK(R.string.survey_submit_ok, R.string.survey_submit_btn_close),
+    SURVEY_ALREADY_SEND(R.string.survey_submit_already_sent, R.string.survey_submit_btn_close),
+    SURVEY_WAS_CHANGED(R.string.survey_submit_was_changed, R.string.survey_submit_btn_close),
+    SURVEY_ERROR(R.string.survey_submit_error, R.string.survey_submit_btn_close);
+
+    private  int labelTxtId;
+    private  int buttonTxtId;
+
+    SurveySubmitResultType(@StringRes int labelId, @StringRes int buttonTxtId) {
+        this.labelTxtId = labelId;
+        this.buttonTxtId = buttonTxtId;
+    }
+
+    public int getLabelTxtId() {
+        return labelTxtId;
+    }
+
+    public int getButtonTxtId() {
+        return buttonTxtId;
+    }
+}
