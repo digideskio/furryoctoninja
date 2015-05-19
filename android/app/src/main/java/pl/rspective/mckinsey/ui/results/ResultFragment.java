ef24b0a7828b5c8eed898fad084f407700b91ac9
LukaszWiktor <lukasz.wiktor@gmail.com>
2015-05-19 14:38:42 +0200

    fixed IllegalArgumentException when max answers count is odd

--- android/app/src/main/java/pl/rspective/mckinsey/ui/results/ResultFragment.java
+++ android/app/src/main/java/pl/rspective/mckinsey/ui/results/ResultFragment.java
@@ -191,20 +191,31 @@ public class ResultFragment extends Fragment implements IFormView, FormQuestionF
         mBarChart.setSetSpacing(Tools.fromDpToPx(3));
         mBarChart.setBarSpacing(Tools.fromDpToPx(14));
 
+        int max = findMaxAnswersCount(question);
+
+        if (max % 2 == 1) {
+            max += 1; // avoid IllegalArgumentException: Step value must be a divisor of distance between minValue and maxValue
+        }
+
+        mBarChart.setBorderSpacing(0)
+                .setAxisBorderValues(0, max + (max % 2), 2) //
+                .setGrid(BarChartView.GridType.FULL, mBarGridPaint)
+                .setYAxis(false)
+                .setXLabels(XController.LabelPosition.OUTSIDE)
+                .setYLabels(YController.LabelPosition.NONE)
+                .show(new Animation());
+    }
+
+    private int findMaxAnswersCount(Question question) {
         int max = 0;
+
         for (Answer answer : question.getAnswers()) {
             if (answer.getCount() > max) {
                 max = answer.getCount();
             }
         }
 
-        mBarChart.setBorderSpacing(0)
-                .setAxisBorderValues(0, max, 2)
-                .setGrid(BarChartView.GridType.FULL, mBarGridPaint)
-                .setYAxis(false)
-                .setXLabels(XController.LabelPosition.OUTSIDE)
-                .setYLabels(YController.LabelPosition.NONE)
-                .show(new Animation());
+        return max;
     }
 
 
