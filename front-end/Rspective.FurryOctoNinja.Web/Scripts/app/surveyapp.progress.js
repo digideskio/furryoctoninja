fde431a2fb9231789f377f0f86380500103585b0
Bandro <bandraszyk@gmail.com>
2015-05-19 19:30:59 +0200

    Implemented Progress view

--- front-end/Rspective.FurryOctoNinja.Web/Scripts/app/surveyapp.progress.js
+++ front-end/Rspective.FurryOctoNinja.Web/Scripts/app/surveyapp.progress.js
@@ -7,13 +7,11 @@
     function progress(dataRefresher, api, progress) {
         var self = this;
 
-        self.json = JSON.stringify(progress, null, 4);
         self.progress = progress;
 
         dataRefresher.addTemporary(function () {
             api.survey.progress()
                 .then(function (progress) {
-                    self.json = JSON.stringify(progress, null, 4);
                     self.users = progress;
                 });
         }, 45);
