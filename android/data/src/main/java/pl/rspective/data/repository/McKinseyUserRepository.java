861a101465ff89fd788e6cf2dc12d70cebe58fe6
marcin.polak <mpolak87@gmail.com>
2015-05-20 19:33:51 +0200

    User logout - clear local data

--- android/data/src/main/java/pl/rspective/data/repository/McKinseyUserRepository.java
+++ android/data/src/main/java/pl/rspective/data/repository/McKinseyUserRepository.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 
 import javax.inject.Inject;
 
-import pl.rspective.data.entity.User;
 import pl.rspective.data.entity.UserPrefs;
 import pl.rspective.data.rest.model.LoginRequest;
 import pl.rspective.data.rest.model.UserRole;
@@ -23,26 +22,26 @@ public class McKinseyUserRepository implements UserRepository {
 
     @Override
     public void saveUser(UserPrefs userPrefs) {
-        if (userPrefs != null) {
             preferences.edit()
                     .putString(USER_TOKEN_KEY, userPrefs.getToken())
                     .putString(USER_ROLE_KEY, userPrefs.getRole().name())
                     .commit();
-        } else {
-            preferences.edit()
-                    .remove(USER_TOKEN_KEY)
-                    .remove(USER_ROLE_KEY)
-                    .commit();
-        }
-
     }
 
     @Override
     public UserPrefs loadUser() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setToken(preferences.getString(USER_TOKEN_KEY, UserPrefs.USER_DEFAULT.getToken()));
-        userPrefs.setRole(UserRole.valueOf(preferences.getString(USER_ROLE_KEY,UserPrefs.USER_DEFAULT.getRole().name())));
+        userPrefs.setRole(UserRole.valueOf(preferences.getString(USER_ROLE_KEY, UserPrefs.USER_DEFAULT.getRole().name())));
         return userPrefs;
+    }
+
+    @Override
+    public void clearUser() {
+        preferences.edit()
+                .putString(USER_TOKEN_KEY, UserPrefs.USER_DEFAULT.getToken())
+                .putString(USER_ROLE_KEY, UserPrefs.USER_DEFAULT.getRole().name())
+                .commit();
     }
 
     @Override
