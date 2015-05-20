c1b062f4076171baf13c1307a0c7f07943247215
pawelrychlik <pawel.rychlik@rspective.pl>
2015-05-20 17:27:01 +0200

    switching to MPAndroidChart

--- android/app/src/main/java/pl/rspective/mckinsey/ui/results/ResultFragment.java
+++ android/app/src/main/java/pl/rspective/mckinsey/ui/results/ResultFragment.java
@@ -1,335 +1,95 @@
 package pl.rspective.mckinsey.ui.results;
 
-import android.animation.TimeInterpolator;
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-import com.db.chart.Tools;
-import com.db.chart.listener.OnEntryClickListener;
-import com.db.chart.model.Bar;
-import com.db.chart.model.BarSet;
-import com.db.chart.view.BarChartView;
-import com.db.chart.view.XController;
-import com.db.chart.view.YController;
-import com.db.chart.view.animation.Animation;
-import com.ogaclejapan.smarttablayout.SmartTabLayout;
-import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
 
-import javax.inject.Inject;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import pl.rspective.data.entity.Answer;
-import pl.rspective.data.entity.Question;
-import pl.rspective.data.entity.Survey;
-import pl.rspective.data.repository.SurveyRepository;
 import pl.rspective.mckinsey.R;
-import pl.rspective.mckinsey.architecture.bus.events.SurveyResultsUpdateEvent;
-import pl.rspective.mckinsey.dagger.Injector;
-import pl.rspective.mckinsey.data.model.SurveySubmitResultType;
-import pl.rspective.mckinsey.mvp.presenters.IFormPresenter;
-import pl.rspective.mckinsey.mvp.views.IFormView;
-import pl.rspective.mckinsey.ui.form.FormQuestionFragment;
-import pl.rspective.mckinsey.ui.form.adapter.FormQuestionPagerAdapter;
-import rx.functions.Action1;
 
-public class ResultFragment extends Fragment implements IFormView, FormQuestionFragment.QuestionListener {
 
-    @Inject
-    Bus bus;
+public class ResultFragment extends SimpleFragment implements OnChartGestureListener {
 
-    @Inject
-    IFormPresenter formPresenter;
-
-    @InjectView(R.id.viewpager)
-    ViewPager viewPager;
-
-    @InjectView(R.id.viewpagertab)
-    SmartTabLayout smartTabLayout;
-
-    private FormQuestionPagerAdapter adapter;
-
-    @Inject
-    SurveyRepository surveyRepository;
-
-    private Survey surveyResult;
-    private int idx;
-
-    public static ResultFragment newInstance() {
+    public static Fragment newInstance() {
         return new ResultFragment();
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Injector.inject(this);
-
-        formPresenter.onResume(this);
-    }
+    private BarChart mChart;
 
     @Override
-    public void onResume() {
-        super.onResume();
-        bus.register(this);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_simple_bar, container, false);
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        bus.unregister(this);
-    }
+        // create a new chart object
+        mChart = new BarChart(getActivity());
+        mChart.setDescription("");
+        mChart.setOnChartGestureListener(this);
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_form_results, container, false);
-        ButterKnife.inject(this, view);
-        return view;
-    }
+        MyMarkerView mv = new MyMarkerView(getActivity(), R.layout.custom_marker_view);
 
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        mChart.setMarkerView(mv);
 
-        adapter = new FormQuestionPagerAdapter(getFragmentManager(), this);
-        smartTabLayout.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
+        mChart.setHighlightIndicatorEnabled(false);
 
-                if(mBarTooltip != null) {
-                    dismissBarTooltip(-1, -1, null);
-                }
+        mChart.setDrawGridBackground(false);
+        mChart.setDrawBarShadow(false);
 
-                idx = position;
-                updateBarChart(idx);
-            }
-        });
+        mChart.setData(generateBarData(1, 20000, 12));
 
-        initBarChart();
+        Legend l = mChart.getLegend();
 
-        formPresenter.loadSurvey();
+        YAxis leftAxis = mChart.getAxisLeft();
 
-        surveyRepository.fetchSurveyResults()
-                .subscribe(new Action1<Survey>() {
-                    @Override
-                    public void call(Survey surveyResult) {
-                        ResultFragment.this.surveyResult = surveyResult;
-                        updateBarChart(idx);
-                    }
-                }, new Action1<Throwable>() {
-                    @Override
-                    public void call(Throwable throwable) {
+        mChart.getAxisRight().setEnabled(false);
 
-                    }
-                });
-    }
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setEnabled(false);
 
-    @InjectView(R.id.barchart)
-    BarChartView mBarChart;
-    private Paint mBarGridPaint;
-    private TextView mBarTooltip;
+        // programatically add the chart
+        FrameLayout parent = (FrameLayout) v.findViewById(R.id.parentLayout);
+        parent.addView(mChart);
 
-    private final TimeInterpolator enterInterpolator = new DecelerateInterpolator(1.5f);
-    private final TimeInterpolator exitInterpolator = new AccelerateInterpolator();
-
-    private final OnEntryClickListener barEntryListener = new OnEntryClickListener(){
-        @Override
-        public void onClick(int setIndex, int entryIndex, Rect rect) {
-            if(mBarTooltip == null)
-                showBarTooltip(setIndex, entryIndex, rect);
-            else
-                dismissBarTooltip(setIndex, entryIndex, rect);
-        }
-    };
-
-    private final View.OnClickListener barClickListener = new View.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            if(mBarTooltip != null)
-                dismissBarTooltip(-1, -1, null);
-        }
-    };
-
-    private final View.OnTouchListener barChartDelegateSwipeListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            mBarChart.onTouchEvent(motionEvent);
-            return viewPager.dispatchTouchEvent(motionEvent);
-        }
-    };
-
-	/*------------------------------------*
-	 *              BARCHART              *
-	 *------------------------------------*/
-
-    private void initBarChart(){
-        mBarChart.setOnEntryClickListener(barEntryListener);
-        mBarChart.setOnClickListener(barClickListener);
-        mBarChart.setOnTouchListener(barChartDelegateSwipeListener);
-
-        mBarGridPaint = new Paint();
-        mBarGridPaint.setColor(this.getResources().getColor(R.color.bar_grid));
-        mBarGridPaint.setStyle(Paint.Style.STROKE);
-        mBarGridPaint.setAntiAlias(true);
-        mBarGridPaint.setStrokeWidth(Tools.fromDpToPx(.75f));
-    }
-
-    private void updateBarChart(int idx) {
-        mBarChart.reset();
-
-        Question question = surveyResult.getQuestions().get(idx);
-
-        BarSet barSet = new BarSet();
-        for (int i = 0; i < question.getAnswers().size(); i++) {
-            Bar bar = new Bar(String.valueOf("abcdefgh".charAt(i)), question.getAnswers().get(i).getCount());
-            bar.setColor(this.getResources().getColor(R.color.bar_fill));
-            barSet.addBar(bar);
-        }
-        mBarChart.addData(barSet);
-
-        mBarChart.setSetSpacing(Tools.fromDpToPx(3));
-        mBarChart.setBarSpacing(Tools.fromDpToPx(14));
-
-        int max = findMaxAnswersCount(question);
-
-        mBarChart.setBorderSpacing(0)
-                .setAxisBorderValues(0, max + (max % 2), 2) //
-                .setGrid(BarChartView.GridType.FULL, mBarGridPaint)
-                .setYAxis(false)
-                .setXLabels(XController.LabelPosition.OUTSIDE)
-                .setYLabels(YController.LabelPosition.NONE)
-                .show();
-    }
-
-    private int findMaxAnswersCount(Question question) {
-        int max = 0;
-
-        for (Answer answer : question.getAnswers()) {
-            if (answer.getCount() > max) {
-                max = answer.getCount();
-            }
-        }
-
-        return max;
-    }
-
-
-    @SuppressLint("NewApi")
-    private void showBarTooltip(int setIndex, int entryIndex, Rect rect){
-        mBarTooltip = (TextView) LayoutInflater.from(getActivity()).inflate(R.layout.bar_tooltip, null);
-        mBarTooltip.setText(Integer.toString(surveyResult.getQuestions().get(idx).getAnswers().get(entryIndex).getCount()));
-        // FIXME how to eliminate dependency to idx?
-
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(rect.width(), rect.height());
-        layoutParams.leftMargin = rect.left;
-        layoutParams.topMargin = rect.top;
-        mBarTooltip.setLayoutParams(layoutParams);
-
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1){
-            mBarTooltip.setAlpha(0);
-            mBarTooltip.setScaleY(0);
-            mBarTooltip.animate()
-                    .setDuration(200)
-                    .alpha(1)
-                    .scaleY(1)
-                    .setInterpolator(enterInterpolator);
-        }
-
-        mBarChart.showTooltip(mBarTooltip);
-    }
-
-
-    @SuppressLint("NewApi")
-    private void dismissBarTooltip(final int setIndex, final int entryIndex, final Rect rect){
-
-        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN){
-            mBarTooltip.animate()
-                    .setDuration(100)
-                    .scaleY(0)
-                    .alpha(0)
-                    .setInterpolator(exitInterpolator).withEndAction(new Runnable(){
-                @Override
-                public void run() {
-                    mBarChart.removeView(mBarTooltip);
-                    mBarTooltip = null;
-                    if(entryIndex != -1)
-                        showBarTooltip(setIndex, entryIndex, rect);
-                }
-            });
-        }else{
-            mBarChart.dismissTooltip(mBarTooltip);
-            mBarTooltip = null;
-            if(entryIndex != -1)
-                showBarTooltip(setIndex, entryIndex, rect);
-        }
+        return v;
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        formPresenter.onDestroy();
+    public void onChartLongPressed(MotionEvent me) {
+        Log.i("LongPress", "Chart longpressed.");
     }
 
     @Override
-    public void updateUi(Survey survey) {
-        if(survey == null || survey.getQuestions() == null) {
-            return;
-        }
-
-        adapter.updateData(survey.getQuestions());
-        viewPager.setAdapter(adapter);
-        smartTabLayout.setViewPager(viewPager);
+    public void onChartDoubleTapped(MotionEvent me) {
+        Log.i("DoubleTap", "Chart double-tapped.");
     }
 
     @Override
-    public void showSubmitButton() {
-
+    public void onChartSingleTapped(MotionEvent me) {
+        Log.i("SingleTap", "Chart single-tapped.");
     }
 
     @Override
-    public void showResultFragment() {
-
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+        Log.i("Fling", "Chart flinged. VeloX: " + velocityX + ", VeloY: " + velocityY);
     }
 
     @Override
-    public void showSubmitDialog(SurveySubmitResultType type) {
-
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+        Log.i("Scale / Zoom", "ScaleX: " + scaleX + ", ScaleY: " + scaleY);
     }
 
     @Override
-    public void nextQuestion(int position) {
-
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+        Log.i("Translate / Move", "dX: " + dX + ", dY: " + dY);
     }
 
-    @Override
-    public Context getViewContext() {
-        return getActivity();
-    }
-
-    @Override
-    public void onQuestionUpdate(int number, Question question, Answer answer) {
-        formPresenter.updateSurvey(number, question, answer);
-    }
-
-    @Subscribe
-    public void onSurveyResultsUpdateEvent(SurveyResultsUpdateEvent updateEvent) {
-        formPresenter.loadSurvey();
-    }
 }
